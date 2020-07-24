5c61be97bdd14c81c54df4db86ba0c8f6f5ef48c
alligatorYE <alligator_@outlook.com>
2020-07-24 16:54:01 +0800

    update

--- 02.Git/Git.md
+++ 02.Git/Git.md
@@ -7,33 +7,36 @@
 ### 提交更新
 
 ```bash
-$git add -u
+git add -u
 ```
 
 ### 重命名文件
 
 ```bash
-$git mv readme README.md
+git mv readme README.md
 ```
 
 ### 查看版本演变历史
 
 ```bash
-$git log
+git log
 
-$git log --oneline		#集中显示记录
+git log --oneline		#集中显示记录
 
-$git log -n4			#查看最近四条记录
+git log -n4			#查看最近四条记录
 
-$git log -n4 --oneline	#集中显示最近四条记录
+git log -n4 --oneline	#集中显示最近四条记录
 
-$git log --all --graph	#显示所有分支树
+git log --all --graph	#显示所有分支树
 
 ```
 
 ### 浏览器查看gitb帮助
 
 ```bash
-$git help --web log
+git help --web log
 ```
-
+### 本地分支关联远程分支
+```bash
+git branch --set-upstream-to=origin/dev
+```
--- src/letsjava/chapter22/scoremaster/ScoreMaster.java
+++ src/letsjava/chapter22/scoremaster/ScoreMaster.java
@@ -1,18 +1,27 @@
+package letsjava.chapter22.scoremaster;
+
 import java.util.Scanner;
 
+/**
+ * @Description
+ * @Version v1.0
+ * @Author yange1
+ * @Date 2020/7/24
+ * @Time 15:04
+ */
 public class ScoreMaster {
     public static void main(String[] args) {
         //Declare six variables, each representing six disciplines
         int ChineseIndex = 0;
-        int MathIndex = 0;
-        int EnglishIndex = 0;
-        int PhysicsIndex = 0;
-        int ChemistryIndex = 0;
-        int BiologyIndex = 0;
+        int MathIndex = 1;
+        int EnglishIndex = 2;
+        int PhysicsIndex = 3;
+        int ChemistryIndex = 4;
+        int BiologyIndex = 5;
 
         int totalDisciplinesCount = 6;
         //Title of each disciplines
-        String[] title = new Stirng[totalDisciplinesCount];
+        String[] title = new String[totalDisciplinesCount];
         title[ChineseIndex] = "Chinese";
         title[MathIndex] = "Math";
         title[EnglishIndex] = "English";
@@ -22,18 +31,21 @@ public class ScoreMaster {
 
         //Get years count of having score recored from scanner
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("How many years have a scores record. ");
         int yearCount = scanner.nextInt();
         double[][] scores = new double[yearCount][totalDisciplinesCount];
         //Print scors for each discipline in each year.
         for (int i = 0; i < yearCount; i++) {
+            System.out.println("----------------------------------year " + (i + 1) + "----------------------------------");
             for (int j = 0; j < totalDisciplinesCount; j++) {
                 scores[i][j] = 80 + Math.random() * 20;
                 System.out.println("The " + title[j] + " score for year " + (i + 1) + " is " + scores[i][j]);
             }
         }
+
         boolean cont = true;
         while (cont) {
+            System.out.println();
             System.out.println("Please select your operation below");
             System.out.println(
                     "1: Seeking the best score of a year\n" +
@@ -45,7 +57,8 @@ public class ScoreMaster {
             int year = 0;
             switch (oprtId) {
                 case 1:
-                    System.out.println("Which year\'s best score you want to know?");
+                    System.out.println("Which year's best score you want to know?");
+                    year = scanner.nextInt();
                     if (year <= 0 || year > yearCount) {
                         System.out.println("invalid input: " + year);
                         cont = false;
@@ -53,15 +66,17 @@ public class ScoreMaster {
                     }
                     year -= 1;
                     int aYearsBestScoreId = 0;
-                    for (int i = 0; i < aYearsBestScoreId; i++) {
+                    for (int i = 0; i < year; i++) {
+//                        System.out.println("scores[year][aYearsBestScoreId]=" + scores[year][aYearsBestScoreId] + ",year="+ year+ ",aYearsBestScoreId="+aYearsBestScoreId);
+//                        System.out.println( scores[year][i]);
                         if (scores[year][aYearsBestScoreId] < scores[year][i]) {
                             aYearsBestScoreId = i;
                         }
                     }
-                    System.out.println("The best score for year " + (year + 1) + " is:" + scores[year][aYearsBestScoreId]);
+                    System.out.println("The best score for year " + (year + 1) + " is " + scores[year][aYearsBestScoreId]);
                     break;
                 case 2:
-                    System.out.println("Which year\'s average score you want to know?");
+                    System.out.println("Which year's average score you want to know?");
                     year = scanner.nextInt();
                     if (year <= 0 || year > yearCount) {
                         System.out.println("invalid input: " + year);
@@ -74,24 +89,26 @@ public class ScoreMaster {
                         avgScoreInAYear += scores[year][i];
                     }
                     avgScoreInAYear /= totalDisciplinesCount;
-                    System.out.println("The average score for year " + (year + 1) + " is: " + avgScoreInAYear);
+                    System.out.println("The average score for year " + (year + 1) + " is " + avgScoreInAYear);
                     break;
                 case 3:
                     int bestYearId = 0;
                     int bestScoreId = 0;
                     for (int i = 0; i < yearCount; i++) {
                         for (int j = 0; j < totalDisciplinesCount; j++) {
+//                            System.out.println("scores[bestYearId][bestScoreId] = " + scores[bestYearId][bestScoreId] + "\tbestYearId=" + bestYearId + ",\tbestScoreId=" + bestScoreId);
+//                            System.out.println("scores[" + i + "][" + j + "] = " + scores[i][j] + "\t");
                             if (scores[bestYearId][bestScoreId] < scores[i][j]) {
                                 bestYearId = i;
-                                bestYearId = j;
+                                bestScoreId = j;
                             }
                         }
                     }
-                    System.out.println("The best score of all years is" + title[bestScoreId] + " in year" + (bestYearId + 1) + ", with a score of" + scores[bestYearId][bestScoreId]);
+                    System.out.println("The best score of all years is " + title[bestScoreId] + " in year " + (bestYearId + 1) + ", with a score of " + scores[bestYearId][bestScoreId]);
                     break;
                 case 4:
-                    System.out.println("Which discipline\'s best score over the year you want to know?");
-                    int disciplineId = 0;
+                    System.out.println("Which discipline's best score over the year you want to know?");
+                    int disciplineId = scanner.nextInt();
                     if (disciplineId <= 0 || disciplineId > totalDisciplinesCount) {
                         System.out.println("invalid input: " + disciplineId);
                         cont = false;
